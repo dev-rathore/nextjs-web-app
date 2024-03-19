70df41957e108f37ab6e57a33cbd49c62e84d29f
dev-rathore <rathoredvd3001@gmail.com>
2024-03-19 23:39:48 +0530

    login api integration

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,24 +3,37 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login response:", response.data);
+      router.push("/profile");
+    } catch (error) {
+      console.error("Error logging in:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Login {loading ? "..." : ""}</h1>
       <hr />
       <label htmlFor="email">Email</label>
       <input
         id="email"
+        className="text-black"
         type="email"
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
@@ -29,6 +42,7 @@ export default function LoginPage() {
       <label htmlFor="password">Password</label>
       <input
         id="password"
+        className="text-black"
         type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
