1bfe3dc141a9b20f3b8f442b7de8813620566eac
dev-rathore <rathoredvd3001@gmail.com>
2024-03-20 00:46:21 +0530

    logout implementation

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      router.push("/login");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <hr />
+      <button
+        onClick={onLogout}
+      >
+        Logout
+      </button>
     </div>
   );
 };
